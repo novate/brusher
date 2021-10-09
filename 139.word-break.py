798f73abd5796e09f6c4539df6e3fe99fa655859
novate <22500949+novate@users.noreply.github.com>
2021-10-09 15:17:34 -0400

    Update

--- 139.word-break.py
+++ 139.word-break.py
@@ -1,12 +1,13 @@
 from typing import List
+from collections import deque
 
 
-class Solution:
+class SolutionMemoization:
     def __init__(self):
         self.word_set = set()
         self.ans_hash = {}
 
-    def wordBreakMemoization(self, s: str, wordDict: List[str]) -> bool:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if len(self.word_set) == 0:
             self.word_set = set(wordDict)
         if s in self.word_set:
@@ -23,3 +24,37 @@ class Solution:
         self.ans_hash[len(s)] = result
 
         return result
+
+
+class SolutionBFS:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        q = deque()
+        visited = set()
+
+        q.append(0)
+        while q:
+            start = q.popleft()
+            if start in visited:
+                continue
+            for end in range(start+1, len(s)+1):
+                if s[start:end] in word_set:
+                    q.append(end)
+                    if end == len(s):
+                        return True
+                visited.add(start)
+        return False
+
+
+class SolutionDP:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False for _ in range(len(s)+1)]
+        dp[0] = True
+
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
