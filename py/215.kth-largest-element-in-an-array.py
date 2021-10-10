bbb7c28f2eafcb4cb73438bde5545b15ff27d3ef
novate <22500949+novate@users.noreply.github.com>
2021-10-10 19:54:31 -0400

    Update

--- py/215.kth-largest-element-in-an-array.py
+++ py/215.kth-largest-element-in-an-array.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 import heapq
 
@@ -5,3 +6,33 @@ import heapq
 class SolutionHeap:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return heapq.nlargest(k, nums)[-1]
+
+
+class SolutionQuickSelect:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quickSelect(nums, len(nums)-k, 0, len(nums)-1)
+
+    def quickSelect(self, nums, k, l, r):
+        if l < r:
+            pivot = self.partition(nums, l, r)
+            if pivot == k:
+                return nums[pivot]
+            elif pivot > k:
+                return self.quickSelect(nums, k, l, pivot-1)
+            else:
+                return self.quickSelect(nums, k, pivot+1, r)
+        else:
+            return nums[k]
+
+    def partition(self, nums, l, r):
+        rnd = randint(l, r)
+        nums[rnd], nums[r] = nums[r], nums[rnd]
+        pivot = r
+        while l < r:
+            while l < r and nums[l] < nums[pivot]:
+                l += 1
+            while l < r and nums[r] >= nums[pivot]:
+                r -= 1
+            nums[l], nums[r] = nums[r], nums[l]
+        nums[r], nums[pivot] = nums[pivot], nums[r]
+        return r
--- py/912.sort-an-array.py
+++ py/912.sort-an-array.py
@@ -2,7 +2,7 @@ from typing import List
 from random import randint
 
 
-class SolutionQuickSort:
+class SolutionQuickSortOnePtr:
     def sortArray(self, nums: List[int]) -> List[int]:
         self.quickSort(nums, 0, len(nums)-1)
         return nums
@@ -28,3 +28,55 @@ class SolutionQuickSort:
 
         nums[i], nums[upper] = nums[upper], nums[i]
         return i
+
+
+class SolutionQuickSortDoublePtr:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSort(nums, 0, len(nums)-1)
+        return nums
+
+    def quickSort(self, nums, l, r):
+        if l < r:
+            rnd = randint(l, r)
+            nums[rnd], nums[r] = nums[r], nums[rnd]
+            p = self.partition(nums, l, r)
+            self.quickSort(nums, l, p-1)
+            self.quickSort(nums, p+1, r)
+
+    def partition(self, nums, l, r):
+        pivot = r
+        while l < r:
+            while l < r and nums[l] < nums[pivot]:
+                l += 1
+            while l < r and nums[r] >= nums[pivot]:
+                r -= 1
+            nums[l], nums[r] = nums[r], nums[l]
+        nums[r], nums[pivot] = nums[pivot], nums[r]
+        return r
+
+
+class SolutionMerge:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        nums_1 = self.sortArray(nums[:mid])
+        nums_2 = self.sortArray(nums[mid:])
+        return self.merge(nums_1, nums_2)
+
+    def merge(self, nums_1, nums_2):
+        nums = []
+        i = 0
+        j = 0
+        while i < len(nums_1) and j < len(nums_2):
+            if nums_1[i] < nums_2[j]:
+                nums.append(nums_1[i])
+                i += 1
+            else:
+                nums.append(nums_2[j])
+                j += 1
+        if i != len(nums_1):
+            nums += nums_1[i:]
+        if j != len(nums_2):
+            nums += nums_2[j:]
+        return nums
